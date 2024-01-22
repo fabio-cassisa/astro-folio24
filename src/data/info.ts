bcc61f2e289f7fc116288b25d696bb587990e6cc
Fabio <cassisafabio97@gmail.com>
2024-01-22 23:02:13 +0100

    updated info.ts

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,73 +2,110 @@ export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
   name: "Fabio Cassisa",
   jobDescription: "Design Technologist/Front-end Developer",
-  about: `I'm a designer/front-end developer based in Europe, you usually find me in Malmö, Copenaghen or Turin.
-   I started as a Product Designer, then after my graduation I shifted more and more into HCI and Developing. I got varied experience in product design, ux
-   design and web development.
+  about: `I am a designer/front-end developer based in Europe, you usually find me in Malmö, Copenaghen or Turin.
+   In my whole life I have always been fascinated by design and technology. I started as a product designer graduating in Politecnico di Torino. I moved forward learning more into the field of HCI at KTH (during my exchange) and IxD at Malmö Uni. 
+   Finally gravitating towards front-end developing and creative coding, graduating in my last YH Bootcamp at Technigo, Stockholm. 
+   I want to get new experiences within Web Development, Product Design or UX Design. I know and have knowledge around design processes, agile methodologies, research methodologies.
+   Design tools I use daily: Figma, Adobe CC, Affinity Designer, Blender, Rhinoceros. 
+   Tech stack I play around the most: HTML, CSS (Tailwind CSS, SASS), Javascript, React, GSAP.js, D3.js, Git Version Control. I am learning: Astro, Next.js, Typescript, Node.js. 
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
+      name: "4Foodies, SRL",
+      location: "Turin, Italy",
+      startDate: "Jan 2021",
       endDate: "Present",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "- Designing the brand, making the logo and its brand guidelines",
+        "- Designing & prototyping a mobile application using Figma",
+        "- Working making a first website based in Wordpress with some custom PHP",
+        "- Developing now a new SPA landing for the project -ongoing",
+        "- Working on a custom web application learning Python (for Django)",
+        "- Working on a custom web application learning some basics of Ruby (for Ruby on Rails)",
       ],
     },
 
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "Design Disciplin",
+      location: "Malmö, Sweden",
+      startDate: "Aug 2022",
+      endDate: "Jan 2023",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "- Used Design Research Methods to gather insights for Design Navigator: a tool to guide designers/developers for their future career",
+        "- Produced PP presentations on Figma and Keynotes",
+        "- Worked on UI elements for the main websites: Design Disciplin",
       ],
     },
 
     {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
+      name: "Art Value",
+      location: "Stockholm, Sweden",
+      startDate: "Apr 2021",
+      endDate: "Dec 2021",
       description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "- Produced 2D and 3D content for an NFT blockchain project promoted by KTH incubators",
+        "- Worked side-by-side with a Solidity developer learning some basics of it",
+        "- Worked with Wordpress and some HTML, CSS, Javascript",
+      ],
+    },
+    {
+      name: "IKEA",
+      location: "Collegno, Italy",
+      startDate: "Feb 2021",
+      endDate: "Jul 2021",
+      description: [
+        "- Worked in the ComIn dept., design team",
+        "- Worked with Autodesk 3ds Max with the IKEA library to create display layouts for the store",
+        "- Managed new product launches, produced new display furniture (designed and then built with wood)",
+      ],
+    },
+    {
+      name: "Pangramma",
+      location: "Turin, Italy",
+      startDate: "Sep 2020",
+      endDate: "Jan 2021",
+      description: [
+        "- Worked in a team of designers and developers with Agile and Scrum methodologies",
+        "- Produced different assets with Adobe XD, Adobe Photoshop and Adobe Illustrator",
+        "- Worked with clients for their websites: wireframing in Adobe XD and developing with Wordpress and custom code",
+        "- Experimented web animations, data viz, 2D and 3D elements using GSAP.js, Three.js and d3.js",
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
+      name: "YH, Technigo",
+      location: "Stockholm, Sweden",
+      startDate: "2023",
+      endDate: "2023",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "Javascript & React Course, Up-skill CS course",
       ],
     },
     {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
+      name: "Malmö University",
+      location: "Malmö, Sweden",
+      startDate: "2021",
+      endDate: "2022",
+      description: [
+        "Master's Degree,  Interaction Design",
+      ],
     },
     {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
+      name: "KTH, Kungliga Tekniska Högskolan",
+      location: "Stockholm, Sweden",
+      startDate: "2019",
+      endDate: "2020",
+      description: ["Master's level courses - Exchange Year, Human Computer Interaction"],
+    },
+    {
+      name: "Politecnico di Torino",
+      location: "Turin, Italy",
+      startDate: "2017",
+      endDate: "2021",
+      description: ["Bachelor's Degree,  Industrial Design & Visual Communication"],
     },
   ],
 
