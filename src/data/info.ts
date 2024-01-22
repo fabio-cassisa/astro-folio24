5d9367e40deb495b5c25e52ef7b9dd400d5d8472
Fabio <cassisafabio97@gmail.com>
2024-01-22 21:37:39 +0100

    pushing some new text

--- src/data/info.ts
+++ src/data/info.ts
@@ -3,13 +3,8 @@ export const info = {
   name: "Fabio Cassisa",
   jobDescription: "Design Technologist/Front-end Developer",
   about: `I'm a designer/front-end developer based in Europe, you usually find me in Malmö, Copenaghen or Turin.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+   I started as a Product Designer, then after my graduation I shifted more and more into HCI and Developing. I got varied experience in product design, ux
+   design and web development.
   `,
 
   experience: [
